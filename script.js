4c00188bbf3b48b738fabab69eff61156ae3fda3
axelmancelon098-svg <axelmancelon098@gmail.com>
2026-02-24 22:35:21 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function troll() {
+  document.body.style.backgroundColor = "hotpink";
+  document.getElementById("title").innerText = "YOU JUST GOT TROLLED 😂";
+  
+  setTimeout(() => {
+    alert("Relax 😎 this is a prank");
+  }, 500);
+}
